beebe8ef11247570f24c8d5b029770438e0f3114
A_Pomales3 <{alfredopomales@gmail.com}>
2018-10-17 20:56:41 -0400

    Update BiggerData/project1/src/main/java/project1/TwitterKeyWordMapper.java

--- BiggerData/project1/src/main/java/project1/TwitterKeyWordMapper.java
+++ BiggerData/project1/src/main/java/project1/TwitterKeyWordMapper.java
@@ -18,26 +18,26 @@ public class TwitterKeyWordMapper extends Mapper<LongWritable, Text, Text, IntWr
             Status status = TwitterObjectFactory.createStatus(rawTweet);
             String tweet = status.getText().toUpperCase();
 
-            // if (tweet.contains("TRUMP")){
-            //     context.write(new Text("TRUMP"), new IntWritable(1));
-            // }
-            if (tweet.contains("Flu")){
-                context.write(new Text("Flu"), new IntWritable(1));
+            if (tweet.contains("TRUMP")){
+                context.write(new Text("TRUMP"), new IntWritable(1));
             }
-            if (tweet.contains("Zika")){
-                context.write(new Text("Zika"), new IntWritable(1));
+            if (tweet.contains("FLU")){
+                context.write(new Text("FLU"), new IntWritable(1));
             }
-            if (tweet.contains("Diarrhea")){
-                context.write(new Text("Diarrhea"), new IntWritable(1));
+            if (tweet.contains("ZIKA")){
+                context.write(new Text("ZIKA"), new IntWritable(1));
             }
-            if (tweet.contains("Ebola")){
-                context.write(new Text("Ebola"), new IntWritable(1));
+            if (tweet.contains("DIARRHEA")){
+                context.write(new Text("DIARRHEA"), new IntWritable(1));
             }
-            if (tweet.contains("Headache")){
-                context.write(new Text("Headache"), new IntWritable(1));
+            if (tweet.contains("EBOLA")){
+                context.write(new Text("EBOLA"), new IntWritable(1));
             }
-            if (tweet.contains("Measles")){
-                context.write(new Text("Measles"), new IntWritable(1));
+            if (tweet.contains("HEADACHE")){
+                context.write(new Text("HEADACHE"), new IntWritable(1));
+            }
+            if (tweet.contains("MEASLES")){
+                context.write(new Text("MEASLES"), new IntWritable(1));
             }
         }
 
