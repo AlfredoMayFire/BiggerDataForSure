229c805e9e74d7b5e7ae3ad65c350d2005d3e96c
A_Pomales3 <{alfredopomales@gmail.com}>
2018-10-17 22:16:37 -0400

    Exercise3

---
+++ BiggerData/project1/src/main/java/project1/UniqueNameDriver.java
@@ -0,0 +1,31 @@
+package project1;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class UniqueNameDriver {
+	public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: UniqueNames <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = new Job();
+        job.setJarByClass(project1.UniqueNameDriver.class);
+        job.setJobName("TwitterUniqueName");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(Mapper.UniqueNameMapper.class);
+        job.setReducerClass(Reducer.UniqueNameReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
